5d4b820e54680d08b6785755742cc67a1658a1e6
laputa12138 <503723212@qq.com>
2025-07-11 17:22:18 +0800

    修改执行脚本

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 python main.py \
     --topic "美国空军人工智能相关技术现状调研、技术成熟度以及未来发展态势评估" \
     --data_path "./data/" \
-    --output_path "output/ADTC-v15-#41-修改了检索召回排序的策略.md" \
+    --output_path "output/ADTC-v16-#43-修改了LLM json解析部分.md" \
     --index_name "abms-V2" \
     --vector_store_path "./my_vector_indexes/" \
     --log_level DEBUG 
